e7ff09c5729a3a5b6a06fc65065329bde23c33de
John Nenniger <JPNENNIGER@GMAIL.com>
2017-10-21 16:46:03 -0700

    started running on http server

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
+const idb = require("idb");
+
 self.addEventListener('install', () => {
-  // const idb = require("idb");
-//
-// const dbPromise = idb.open('keyval-store', 1, upgradeDB => {
-//   upgradeDB.createObjectStore('keyval');
-// });
+
+  const dbPromise = idb.open('keyval-store', 1, upgradeDB => {
+  upgradeDB.createObjectStore('keyval');
+});
 
   console.log('Installation complete.')
 })
