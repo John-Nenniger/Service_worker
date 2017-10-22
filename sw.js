62559f361b8809c47d165013efd0c6f6ae36dee4
John Nenniger <JPNENNIGER@GMAIL.com>
2017-10-22 11:03:24 -0700

    to be merged with Kiki

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-importScripts('/node_modules/idb/lib/idb.js');
+// importScripts('/node_modules/idb/lib/idb.js');
+importScripts('/node_modules/idb-keyval/dist/idb-keyval-min.js');
 
 function createDB(){
   idb.open('cat_data', 1, function(upgradeDB){
@@ -10,26 +11,45 @@ function createDB(){
   });
 }
 
-const dbPromise = idb.open('keyval-store', 1, upgradeDB => {
-  upgradeDB.createObjectStore('keyval');
-});
+function storeData(dataEvent){
+  let data = dataEvent.data;
+
+}
+
+function getData(dataEvent){
+  let data = dataEvent.data;
+
+}
+
+// const dbPromise = idb.open('keyval-store', 1, upgradeDB => {
+//   upgradeDB.createObjectStore('keyval');
+// });
 
 self.addEventListener('install', () => {
-// ^ install the service worker
-  console.log('Installation complete.');
-  // Here I need to send a message to John's server to say that the node is online
+  // ^ install the service worker
+  //normally there would be some caching of assets here, but our site
+  // has almost no assets and this service worker is just for sending and recievign fetches
 })
+
+self.addEventListener('activate', function(event) {
+  // The activate event listener is automatically fired upon installation completing
+  event.waitUntil(
+    createDB()
+  );
+  console.log("activation complete, database created");
+});
 
 self.addEventListener('push', function(event) {
-  console.log(`this is a push event : ${event}`)
-
+  console.log(`this is a push event : ${event}`);
   // Here I need to add an event listener to accept incoming data from John's server
   // here I need to send that data to the database using idb
-})
+});
 
-fetch("Andy_and_Johns_URL")
-  .then()
+self.addEventListener('fetch', function(event){
+  console.log(`this was accepted by fetch`);
+});
 
+fetch('Andy_and_Johns_URL', {})
 
 
 // Here I'll add an event listener for a get request from John's server
